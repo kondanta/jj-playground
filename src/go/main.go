ee195c1d35835eb3d3a923ac72d72f132a565dbb
Taylan Dogan <git@taylandogan.info>
2025-06-01 18:23:13 +0200

    feat: add sum function

--- src/go/main.go
+++ src/go/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func sum(a int, b int) int {
+	return a + b
+}
+
 func main() {
-	fmt.Println("Hello world")
+	// Example usage of the sum function
+	result := sum(3, 5)
+	fmt.Println("The sum is:", result)
 }
